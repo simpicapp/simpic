740e1a9d9759a57fe451e50ca55761e800c8bbd0
Chris Smith <chris@chameth.com>
2020-02-12 23:34:50 +0000

    Allow specifying the offset for timeline items

--- http/timeline.go
+++ http/timeline.go
@@ -4,11 +4,21 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func (s *server) handleTimeline() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		photos, err := s.db.GetPhotosByTime(0, 100)
+		var (
+			offset int
+			err    error
+		)
+		param, ok := r.URL.Query()["offset"]
+		if ok && len(param) > 0 {
+			offset, err = strconv.Atoi(param[0])
+		}
+
+		photos, err := s.db.GetPhotosByTime(offset, 100)
 		if err != nil {
 			log.Printf("unable to retrieve timeline: %v\n", err)
 			w.WriteHeader(http.StatusInternalServerError)
