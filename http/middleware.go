93ddd8ad2c4490639cb1bc2d861e205dc0db81b8
Chris Smith <chris@chameth.com>
2020-02-18 01:27:57 +0000

    Require user to be logged in to upload
    Closes #6

--- http/middleware.go
+++ http/middleware.go
@@ -11,45 +11,53 @@ import (
 const ctxPhoto = "photo"
 const ctxUser = "user"
 
-func (s server) photoContext() func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			id, err := s.idForKey(chi.URLParam(r, "uuid"))
-			if err != nil {
-				writeError(w, http.StatusNotFound, "No such photo")
-				return
-			}
+func (s *server) photoContext(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id, err := s.idForKey(chi.URLParam(r, "uuid"))
+		if err != nil {
+			writeError(w, http.StatusNotFound, "No such photo")
+			return
+		}
 
-			photo, err := s.db.GetPhoto(id)
-			if err != nil {
-				writeError(w, http.StatusNotFound, "No such photo")
-				return
-			}
+		photo, err := s.db.GetPhoto(id)
+		if err != nil {
+			writeError(w, http.StatusNotFound, "No such photo")
+			return
+		}
 
-			ctx := context.WithValue(r.Context(), ctxPhoto, photo)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
+		ctx := context.WithValue(r.Context(), ctxPhoto, photo)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
-func (s server) authenticatedContext() func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			header := r.Header.Get("Authorization")
-			if strings.HasPrefix(header, "Bearer ") {
-				signedToken := strings.TrimPrefix(header, "Bearer ")
-				user, err := s.validateToken(signedToken)
-				if err != nil {
-					log.Printf("client supplied JWT but it was invalid: %v\n", err)
-					writeError(w, http.StatusUnauthorized, "Invalid bearer token supplied")
-					return
-				}
-
-				ctx := context.WithValue(r.Context(), ctxUser, user)
-				next.ServeHTTP(w, r.WithContext(ctx))
-			} else {
-				next.ServeHTTP(w, r)
+func (s *server) authenticatedContext(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		header := r.Header.Get("Authorization")
+		if strings.HasPrefix(header, "Bearer ") {
+			signedToken := strings.TrimPrefix(header, "Bearer ")
+			user, err := s.validateToken(signedToken)
+			if err != nil {
+				log.Printf("client supplied JWT but it was invalid: %v\n", err)
+				writeError(w, http.StatusUnauthorized, "Invalid bearer token supplied")
+				return
 			}
-		})
-	}
+
+			ctx := context.WithValue(r.Context(), ctxUser, user)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func (s *server) requireAnyUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := r.Context().Value(ctxUser)
+		if user == nil {
+			writeError(w, http.StatusUnauthorized, "Authorisation required")
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
--- http/routes.go
+++ http/routes.go
@@ -9,14 +9,18 @@ func (s *server) routes() {
 	s.router.Post("/login", s.handleAuthenticate())
 
 	s.router.Group(func(r chi.Router) {
-		r.Use(s.authenticatedContext())
+		r.Use(s.authenticatedContext)
 
-		r.Post("/photo", s.handleStorePhoto())
+		s.router.Group(func(r chi.Router) {
+			r.Use(s.requireAnyUser)
+			r.Post("/photo", s.handleStorePhoto())
+		})
+
 		r.Get("/timeline", s.handleTimeline())
 	})
 
 	s.router.Group(func(r chi.Router) {
-		r.Use(s.photoContext())
+		r.Use(s.photoContext)
 		r.Get("/thumbnail/{uuid}", s.handleGetThumbnail())
 		r.Get("/photo/{uuid}", s.handleGetPhoto())
 	})
