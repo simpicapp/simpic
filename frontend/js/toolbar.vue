419baaa3b491b392829a28bbc2bb529104327cae
Chris Smith <chris@chameth.com>
2020-02-24 21:13:20 +0000

    Redesign the toolbar
    Issue #34

--- frontend/js/toolbar.vue
+++ frontend/js/toolbar.vue
@@ -1,52 +1,82 @@
 <template>
     <header>
-        <h1>Simpic</h1>
-        <router-link to="/timeline/">Timeline</router-link>
-        <router-link to="/albums/">Albums</router-link>
+        <h1><img src="/res/logo.png" alt="Simpic"></h1>
+        <router-link to="/timeline/"><span>Timeline</span></router-link>
+        <router-link to="/albums/"><span>Albums</span></router-link>
         <span class="grow"></span>
         <button @click="loginClick" v-if="!$root.loggedIn">Login</button>
         <template v-else>
-            <div>Logged in as {{$root.username}}</div>
+            <div class="username">Logged in as {{$root.username}}</div>
             <button @click="logoutClick">Logout</button>
         </template>
     </header>
 </template>
 
 <style lang="scss" scoped>
+    @use '../css/vars';
+
     header {
         display: flex;
         margin: 0 0 20px 0;
-        padding: 10px;
-        border-bottom: 1px solid #ccc;
+        background-color: var(--smaragdine);
         justify-content: space-between;
         align-items: center;
+        box-shadow: 0 1px 2px black;
+        height: 70px;
+        overflow: visible;
 
         h1 {
             text-transform: lowercase;
-            margin-right: 20px;
+            margin: 10px 20px 0 10px;
+            padding: 0;
         }
 
-        span {
+        img {
+            margin-top: 28px;
+            min-height: 66px;
+            height: 66px;
+        }
+
+        .grow {
             flex-grow: 1;
         }
 
         a {
+            align-self: stretch;
             margin: 0 10px;
             padding: 5px;
-            align-self: end;
-            color: black;
+            width: 125px;
+            color: white;
             text-decoration: none;
+            font-size: large;
+            display: flex;
+            align-items: flex-end;
+            justify-content: center;
+            border-left: 1px solid darken(vars.$smaragdine, 10%);
+            border-right: 1px solid darken(vars.$smaragdine, 10%);
 
-            &:hover {
-                color: blue;
-                text-decoration: underline;
-            }
-
-            /*noinspection CssUnusedSymbol*/
-            &.router-link-active {
-                font-weight: bold;
+            &.router-link-active, &:hover {
                 text-decoration: none;
-                color: black;
+                color: white;
+                background: lighten(vars.$smaragdine, 5%);
+            }
+        }
+
+        .username {
+            color: white;
+        }
+
+        button {
+            background-color: transparent;
+            border: 1px solid white;
+            border-radius: 2px;
+            color: white;
+            padding: 5px 20px;
+            cursor: pointer;
+            margin-right: 20px;
+
+            &:hover, &:active {
+                background: lighten(vars.$smaragdine, 5%);
             }
         }
 
