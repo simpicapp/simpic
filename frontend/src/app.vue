3379b07449777214032886221e4add1eb25c8c93
Chris Smith <chris@chameth.com>
2020-03-01 22:25:54 +0000

    Fix input borders in Chromium

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -18,31 +18,6 @@
   @import "assets/css/global";
   @import "assets/css/reset";
 
-  body {
-    font-family: sans-serif;
-    margin: 0;
-    padding: 0;
-  }
-
-  :root {
-    --ease-in-cubic: cubic-bezier(0.55, 0.055, 0.675, 0.19);
-    --ease-out-cubic: cubic-bezier(0.645, 0.045, 0.355, 1);
-    --ease-out-back: cubic-bezier(0.175, 0.885, 0.32, 1.275);
-  }
-
-  input {
-    padding: 5px;
-  }
-
-  input[type="submit"] {
-    padding: 10px;
-  }
-
-  input[type="submit"]:hover,
-  input[type="submit"]:active {
-    background-color: vars.$primary;
-  }
-
   #content {
     min-height: calc(100vh - 90px - 80px);
   }
